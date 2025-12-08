25efb058f8f81adfb2cabae45aca6f58b28d03f9
Нажмутдин Вагабов <54717479+Kizhan96@users.noreply.github.com>
2025-12-08 18:15:30 +0300

    Fix loopback macro and include media header

--- client/src/window.h
+++ client/src/window.h
@@ -14,6 +14,7 @@
 #include "frame_encode_worker.h"
 #include "h264_encoder.h"
 #include "h264_decoder.h"
+#include "types.h"
 #include <QMap>
 #include <QImage>
 #include <QMediaDevices>
@@ -22,6 +23,7 @@
 #include <QMutex>
 #include <QWaitCondition>
 #include <QSet>
+#include <QHash>
 
 class QLineEdit;
 class QPushButton;
@@ -40,6 +42,32 @@ class QAudioSource;
 class QAudioSink;
 class QIODevice;
 
+class VideoDecodeWorker : public QObject {
+    Q_OBJECT
+public:
+    explicit VideoDecodeWorker(QObject *parent = nullptr);
+    ~VideoDecodeWorker() override;
+
+public slots:
+    void processPayload(const QString &sender, const QByteArray &payload);
+    void reset();
+
+signals:
+    void frameReady(const QString &sender, const QImage &image);
+    void streamPresence(const QString &sender);
+    void streamStopped(const QString &sender);
+
+private:
+    struct DecoderState {
+        H264Decoder *decoder = nullptr;
+        quint32 lastFrameId = 0;
+        bool ready = false;
+    };
+    QHash<QString, DecoderState> states;
+    DecoderState &ensureState(const QString &sender);
+    void resetSender(const QString &sender);
+};
+
 class SendWorker : public QObject {
     Q_OBJECT
 public:
@@ -55,9 +83,11 @@ public slots:
 private:
     QTcpSocket *socket = nullptr;
     QQueue<QByteArray> queue;
+    qint64 queueBytes = 0;
     QMutex mutex;
     QWaitCondition cond;
     bool running = true;
+    static constexpr qint64 kMaxQueuedBytes = 1 * 1024 * 1024; // 1MB cap to avoid latency buildup
 };
 
 class ChatWindow : public QWidget {
@@ -69,12 +99,16 @@ public:
 private slots:
     void onConnectClicked();
     void onSendClicked();
+    void onAttachClicked();
     void onSocketReadyRead();
     void onSocketConnected();
     void onSocketDisconnected();
     void onSocketError(QAbstractSocket::SocketError err);
     void onLoginResult(bool ok);
     void onMessageReceived(const QString &sender, const QString &message, const QString &timestamp);
+    void onRemoteFrameReady(const QString &sender, const QImage &image);
+    void onRemoteStreamPresence(const QString &sender);
+    void onRemoteStreamStopped(const QString &sender);
     void onMicToggle();
     void onMuteToggle();
     void onScreenShareStart();
@@ -111,8 +145,13 @@ private:
     void startStreamAudioCapture();
     void stopStreamAudioCapture();
     void ensureStreamAudioOutput();
+    QAudioDevice chooseLoopbackAudio() const;
     void sendStreamStopSignal(int delayMs = 0);
     QByteArray applyVoiceGain(const QByteArray &pcm, qreal gain, const QAudioFormat &format);
+    void handleChatMediaMessage(const Message &msg);
+    void handleMediaControlMessage(const Message &msg);
+    void dispatchVideoPayload(const QString &sender, const QByteArray &payload);
+    void sendMediaState(const QString &kind, const QString &state);
     void sendUdpAnnouncement();
     void handleVoiceUdp();
     void handleVideoUdp();
@@ -121,6 +160,7 @@ private:
 
     QLineEdit *messageEdit;
     QPushButton *sendButton;
+    QPushButton *attachButton;
     QPushButton *micToggleButton;
     QPushButton *muteButton;
     QPushButton *shareToggleButton;
@@ -160,6 +200,8 @@ private:
     int shareFps = 10;
     QSize shareResolution = QSize(1280, 720);
     int shareQuality = 60;
+    int webFrameStride = 3;
+    int webFrameCounter = 0;
     double micVolume = 1.0;
     double outputVolume = 1.0;
     QByteArray inputDeviceId;
@@ -172,22 +214,18 @@ private:
     bool micMuted = false;
     QString currentStreamUser;
     bool isLocalSharingPreviewVisible = false;
-    qint64 lastFrameTimestamp = 0;
     quint32 lastFrameIdSent = 0;
-    QMap<QString, quint32> lastFrameIdReceived;
     bool streamConfigSent = false;
     bool streamPresenceSent = false;
     int framesSinceConfig = 0;
     int framesSincePresence = 0;
     const int configRepeatInterval = 30;
-    bool decoderReady = false;
     bool encodingInProgress = false;
     bool hasPendingFrame = false;
     QImage pendingFrame;
     quint32 pendingFrameId = 0;
     qint64 pendingFrameTimestamp = 0;
     H264Encoder h264Encoder;
-    H264Decoder h264Decoder;
     FrameEncodeWorker *encoderWorker = nullptr;
     QThread encoderThread;
     quint16 voiceSeq = 0;
@@ -195,10 +233,14 @@ private:
 
     SendWorker *sendWorker = nullptr;
     QThread sendThread;
+    VideoDecodeWorker *videoWorker = nullptr;
+    QThread videoThread;
     void updateUserListDisplay();
     QTimer *connectionTimer = nullptr;
     QTimer *pingTimer = nullptr;
     void updatePing();
+    qint64 lastPingSentMs = 0;
+    bool awaitingPong = false;
 
     QMediaDevices mediaDevices;
     // Stream audio (system/loopback) mini-RTP-like
