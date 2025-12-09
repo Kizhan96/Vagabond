79565dfbd0c4033bf8d2528774889921b2bc4a7f
Нажмутдин Вагабов <54717479+Kizhan96@users.noreply.github.com>
2025-12-09 12:15:24 +0300

    Delete client directory

--- client/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "livekit_window.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    LiveKitWindow window;
-    window.show();
-    return app.exec();
-}
