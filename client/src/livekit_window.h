1df4bdf67e656ae93af94dfc8b0cddd148bf11c9
Нажмутдин Вагабов <54717479+Kizhan96@users.noreply.github.com>
2025-12-09 12:11:21 +0300

    Hide log panel and tighten LiveKit UX

---
+++ client/src/livekit_window.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QTabWidget>
+
+class LiveKitWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit LiveKitWindow(QWidget *parent = nullptr);
+
+private slots:
+    void connectToLiveKit();
+    void closeTab(int index);
+
+private:
+    void appendLog(const QString &line);
+
+    QLineEdit *urlInput {nullptr};
+    QLineEdit *tokenInput {nullptr};
+    QLineEdit *roomInput {nullptr};
+    QPushButton *connectButton {nullptr};
+    QLabel *statusLabel {nullptr};
+    QTabWidget *tabWidget {nullptr};
+};
