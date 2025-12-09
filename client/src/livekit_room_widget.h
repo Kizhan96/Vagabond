b9620c31a0877525bf498a657ba6a7f4b646c557
Нажмутдин Вагабов <54717479+Kizhan96@users.noreply.github.com>
2025-12-09 15:14:28 +0300

    Add LiveKit SDK override fallback

--- client/src/livekit_room_widget.h
+++ client/src/livekit_room_widget.h
@@ -7,14 +7,20 @@
 class LiveKitRoomWidget : public QWidget {
     Q_OBJECT
 public:
-    explicit LiveKitRoomWidget(const QString &url, const QString &token, const QString &roomLabel, QWidget *parent = nullptr);
+    explicit LiveKitRoomWidget(const QString &url, const QString &token, const QString &roomLabel,
+                               bool startWithAudio, bool startWithVideo,
+                               const QString &sdkOverride, QWidget *parent = nullptr);
 
     QString title() const { return roomTitle; }
 
 private:
-    QString buildHtml(const QString &url, const QString &token, const QString &roomLabel) const;
+    QString buildHtml(const QString &url, const QString &token, const QString &roomLabel,
+                      const QString &sdkOverride) const;
     QString escapeForJs(const QString &value) const;
 
     QString roomTitle;
     QWebEngineView *webView {nullptr};
+    bool audioEnabled {true};
+    bool videoEnabled {true};
+    QString sdkUrlOverride;
 };
